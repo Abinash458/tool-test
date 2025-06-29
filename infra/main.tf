cff61f6ef9f4b33c8b448ffaba34570e51ae9be9
Abinash Mohapatra <amohapatra458@gmail.com>
2025-06-29 21:35:52 +0530

    Updated infra code via chatbot

--- infra/main.tf
+++ infra/main.tf
@@ -1,24 +1,15 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "main" {
   name     = "example-resource-group"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "main" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
