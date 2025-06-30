fac8a3381118d8dfcd689899901bdeddc7e98fd2
Abinash Mohapatra <amohapatra458@gmail.com>
2025-06-30 10:18:02 +0530

    Added variables.tf for secrets

---
+++ infra/variables.tf
@@ -0,0 +1,55 @@
+variable "secret_var_1" {
+  description = "Sensitive value for secret_var_1"
+  type = string
+}
+
+variable "secret_var_2" {
+  description = "Sensitive value for secret_var_2"
+  type = string
+}
+
+variable "secret_var_3" {
+  description = "Sensitive value for secret_var_3"
+  type = string
+}
+
+variable "secret_var_4" {
+  description = "Sensitive value for secret_var_4"
+  type = string
+}
+
+variable "secret_var_5" {
+  description = "Sensitive value for secret_var_5"
+  type = string
+}
+
+variable "secret_var_6" {
+  description = "Sensitive value for secret_var_6"
+  type = string
+}
+
+variable "secret_var_7" {
+  description = "Sensitive value for secret_var_7"
+  type = string
+}
+
+variable "secret_var_8" {
+  description = "Sensitive value for secret_var_8"
+  type = string
+}
+
+variable "secret_var_9" {
+  description = "Sensitive value for secret_var_9"
+  type = string
+}
+
+variable "secret_var_10" {
+  description = "Sensitive value for secret_var_10"
+  type = string
+}
+
+variable "secret_var_11" {
+  description = "Sensitive value for secret_var_11"
+  type = string
+}
+
